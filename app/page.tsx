c02d39d6b7ac8e97b21652720d0eaf6ebb69cb8c
LucioFider <fidelislucio404@gmail.com>
2023-03-25 15:10:25 +1000

    2nd-commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,13 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main>
-      <h1>Blog Next 13</h1>
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome 👋&nbsp;
+        <span className="whitespace-nowrap">
+          Im <span className="font-bold">Fid</span>
+        </span>
+      </p>
     </main>
   );
 }
